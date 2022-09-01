8c5b042ae7803faf12a9b008d55391a3cbc9f556
willcordeiro <willcordeiro800@gmail.com>
2022-09-01 17:15:51 -0300

    first commit

---
+++ parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -0,0 +1,14 @@
+package com.api.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+}
---
+++ parking-control/src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,9 @@
+package com.api.parkingcontrol.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel {
+    
+}
